d73c41aa6127665e34b4ab454dcfa4fcd2df924e
Maxim Lyutikov <hackmailmaxim@gmail.com>
2025-01-26 16:46:47 +0300

    Readme update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # LinuxLabsChanger
 This repo was created when I understood that it's simpler for me to make a program than replace my name on screenshots manually.
+
+![LinuxLabsChanger GUI](./gui.png)
--- obj/Debug/net6.0-windows/LinuxLabsChanger.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/LinuxLabsChanger.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LinuxLabsChanger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b58f02e2c16475f556f7f53ded608dbd88741dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("LinuxLabsChanger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LinuxLabsChanger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
